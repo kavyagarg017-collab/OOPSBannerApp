a028aefea0b5c4e7b6d02ae7c08656412518e119
kavyagarg017 <kavyagarg017@gmail.com>
2026-03-04 13:36:57 +0530

    UC7 AND UC8

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,48 @@
-public class OOPSBannerApp{
-	public static void main(String[] args) {
-		System.out.println("OOPS");
+class OOPS {
+    public static void main(String[] args) {
+
+        // Declare and initialize String array using String.join()
+        String[] banner = {
+            String.join("   ",
+                " ***** ",  // O
+                " ***** ",  // O
+                " ***** ",  // P
+                " ***** ",  // P
+                " ***** "   // S
+            ),
+            String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+            ),
+            String.join("   ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                " ***** ",
+                " ***** "
+            ),
+            String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*      ",
+                "     * "
+            ),
+            String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** "
+            )
+        };
+
+        // Loop to print each line
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
